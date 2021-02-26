8bd09b14d6d93843437c567cbd004d0717fff39a
demonkiller <hiranyagarbh7@gmail.com>
2021-02-26 23:30:51 +0530

    added bootable usb script

--- bootableusb.sh
+++ bootableusb.sh
@@ -1,8 +1,8 @@
 #Bootable USB Maker for Thakur OS
-#By demonkiller
-#NOTE 1: Put this script in the same folder which has the ISO
-
+#By Demonkiller
 #Warning: Insert One USB at a time only. If you have any other USB either remove them from the system or change the script accordingly. Check size with lsblk (uncommment line 10 & 11).
+
+
 echo "Welcome to Bootable USB Maker!"
 echo "We are making Thakur OS Bootable USB, please wait..."
 echo "                               "
@@ -25,8 +25,9 @@ echo "USB is Formatted.."
 echo "                               "
 echo "================================================================"
 echo "Making Thakur OS bootable ISO, this may take sometime...."
-sudo dd if=/out/ of=/dev/sdb status='progress'
-#NOTE 2: replace ubuntu-20.04-desktop-amd64.iso and you can use this script for making bootable USB of any other distribution.
+cd out
+sudo dd if=thakuros-$(date +%Y.%m.%d).iso of=/dev/sdb status='progress'
+
 
 echo "D O N E! Your USB should be bootable now, with Thakur OS."
 echo "Thank YOU!"
